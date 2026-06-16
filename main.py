434ed6ee132b71b32e133692b2e455ae04d3bcbb
banumrmr-hue <banumrmr@gmail.com>
2026-06-16 14:30:47 +0530

    Update and rename lulia[.py to main.py

--- main.py
+++ main.py
@@ -8,6 +8,23 @@ Requires: BOT_TOKEN and MONGODB_URI environment variables
 # ─────────────────────────────────────────────────────────────────────────────
 #  IMPORTS
 # ─────────────────────────────────────────────────────────────────────────────
+import os 
+os.system("pip install flask")
+from flask import Flask
+import threading
+import asyncio
+
+# 👇 Flask server (TOP PART)
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+def run_web():
+    app.run(host="0.0.0.0", port=10000)
+
+threading.Thread(target=run_web).start()
 
 import ast
 import asyncio
